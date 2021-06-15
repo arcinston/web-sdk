b03db8a1c05fd8efdde134fea2003db5e1c60cec
danisharora099 <danisharora099@gmail.com>
2021-06-15 21:24:16 +0530

    add: audio, video for multiple peers

--- src/App.js
+++ src/App.js
@@ -1,41 +1,40 @@
 import { useEffect, useState, useRef } from "react";
 import HuddleClient from "./lib/huddle-client";
 import emitter from "./lib/utils/EventEmitter";
-import PeerViewport from "./components/PeerViewport";
+import { PeerVideo, PeerAudio } from "./components/PeerViewport";
+import { getTrack } from "./lib/utils/helpers";
+
+const config = {
+  roomId: "dev",
+  peerId: "peer" + Math.floor(Math.random() * 4000),
+  displayName: "s3",
+};
 
 function App() {
   const [huddle, setHuddle] = useState(null);
+  const [roomState, setRoomState] = useState(false);
+  const [micState, setMicState] = useState(false);
+  const [webcamState, setWebcamState] = useState(false);
+
   const [peers, setPeers] = useState([]);
-  const [videoConsumerTracks, setVideoConsumerTracks] = useState([]);
+  const [consumerStreams, setConsumerStreams] = useState({
+    video: [],
+    audio: [],
+  });
   const meVideoElem = useRef(null);
 
   //initialize the app
   useEffect(() => {
-    const config = {
-      roomId: "room",
-      peerId: "peer" + Math.floor(Math.random() * 4000),
-      displayName: "name",
-    };
-
     const myHuddleClient = new HuddleClient(config);
 
     setHuddle(myHuddleClient);
   }, []);
 
-  const getStream = (track) => {
-    const stream = new MediaStream();
-    stream.addTrack(track);
-    return stream;
-  };
-
-  const init = async () => {
-    if (!huddle) return;
-
-    await huddle.join();
-
+  const setupEventListeners = async () => {
     emitter.on("addPeer", (peer) => {
       console.log("new peer =>", peer);
-      setPeers([...peers, peer]);
+      setPeers((_peers) => [..._peers, peer]);
+      console.log("setPeers", peers);
     });
 
     emitter.on("addProducer", (producer) => {
@@ -44,57 +43,154 @@ function App() {
         const videoStream = producer.track;
         if (typeof videoStream == "object") {
           try {
-            meVideoElem.current.srcObject = getStream(videoStream);
+            meVideoElem.current.srcObject = getTrack(videoStream);
           } catch (error) {
             console.error(error);
           }
         }
       } else if (producer.kind === "audio") {
-        //handle for audio producer
+        //TODO: handle my audio producer
       }
     });
 
     emitter.on("addConsumer", (consumer) => {
       console.log("new consumer =>", consumer);
-      const videoStream = consumer.track;
-      if (videoStream.kind === "video") {
-        const videoTrack = getStream(videoStream);
-        setVideoConsumerTracks([...videoConsumerTracks, videoTrack]);
-      } else {
-        //handle for audio consumer
+      if (consumer.track.kind === "video") {
+        //TODO: handle consumer screenshares
+        const videoStream = consumer.track;
+
+        let _peers = peers;
+        console.log({ _peers });
+        for (const _peer of _peers) {
+          if (_peer.id === consumer.peerId) {
+            console.log("yezzir");
+            _peer.consumers.push(videoStream);
+          }
+        }
+        console.log({ _peers });
+        setPeers(_peers);
+
+        console.log(peers);
+
+        setConsumerStreams((prevState) => ({
+          ...prevState,
+          video: [...prevState.video, videoStream],
+        }));
+      } else if (consumer.track.kind === "audio") {
+        const audioStream = consumer.track;
+        setConsumerStreams((prevState) => ({
+          ...prevState,
+          audio: [...prevState.audio, audioStream],
+        }));
       }
+    });
+
+    emitter.on("removeConsumer", ({ consumerId, peerId }) => {
+      // setConsumerStreams((prevState) =>
+      //   prevState.filter((consumer) => consumer.id !== consumerId)
+      // );
+    });
+
+    emitter.on("removePeer", (peerId) => {
+      setPeers((prevState) => prevState.filter((peer) => peer.id !== peerId));
     });
   };
 
-  const _enableWebcam = async () => {
+  const joinRoom = async () => {
     if (!huddle) return;
-    await huddle.enableWebcam();
+
+    try {
+      await huddle.join();
+      setupEventListeners();
+      setRoomState(true);
+    } catch (error) {
+      setRoomState(false);
+      alert(error);
+    }
+  };
+
+  const leaveRoom = async () => {
+    if (!huddle) return;
+    try {
+      await huddle.close();
+      setRoomState(false);
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  //TODO: add pauseWebcam() and resumeWebcam()
+  const enableWebcam = async () => {
+    if (!huddle) return;
+    try {
+      await huddle.enableWebcam();
+      setWebcamState(true);
+    } catch (error) {
+      setWebcamState(false);
+      alert(error);
+    }
+  };
+
+  const disableWebcam = async () => {
+    if (!huddle) return;
+    try {
+      await huddle.disableWebcam();
+      setWebcamState(true);
+    } catch (error) {
+      alert(error);
+      setWebcamState(false);
+    }
+  };
+
+  //TODO: add muteMic() and unmuteMic()
+  const enableMic = async () => {
+    if (!huddle) return;
+    try {
+      huddle.enableMic();
+      setMicState(true);
+    } catch (error) {
+      setMicState(false);
+      alert(error);
+    }
+  };
+
+  const disableMic = async () => {
+    if (!huddle) return;
+    try {
+      huddle.disableMic();
+      setMicState(false);
+    } catch (error) {
+      alert(error);
+      setMicState(true);
+    }
   };
 
   return (
     <div className="App">
-      <button onClick={init}> init </button>
-      <button onClick={_enableWebcam}>Webcam</button>
-      <button onClick={async () => await huddle.enableMic()}>Mic</button>
+      <button onClick={roomState ? leaveRoom : joinRoom}>
+        {roomState ? "Leave Room" : "Join Room"}
+      </button>
+      <button onClick={webcamState ? disableWebcam : enableWebcam}>
+        {webcamState ? "Disable Webcam" : "Enable Webcam"}
+      </button>
+      <button onClick={micState ? disableMic : enableMic}>
+        {micState ? "Disable Mic" : "Enable Mic"}
+      </button>
       <video height="400px" width="400px" autoPlay ref={meVideoElem} />
-      {videoConsumerTracks.map((track, idx) => {
-        return (
-          <>
-            <PeerViewport videoTrack={track} idx={idx} />;
-          </>
-        );
+      {consumerStreams.video.map((stream, idx) => {
+        return <PeerVideo key={idx} videoTrack={getTrack(stream)} />;
+      })}
+      {consumerStreams.audio.map((stream, idx) => {
+        return <PeerAudio key={idx} audioTrack={getTrack(stream)} />;
       })}
     </div>
   );
 }
 
-// join
-// enableWebcam
-// disableWebcam
-// pauseVideo
-// resumeVideo
-// muteMic
-// unmuteMic
-// close
-
 export default App;
+
+//2 peers -> production & consumption of videos
+// audio
+// pause/resume/stop
+
+//2+ peers
