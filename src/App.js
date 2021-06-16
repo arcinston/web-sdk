afdc130b59606563d5ca6ecf18affb2c799070af
danisharora099 <danisharora099@gmail.com>
2021-06-16 14:10:38 +0530

    remove: roomState event listeners

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-// import HuddleClient, { emitter } from "huddle01-client";
+import HuddleClient, { emitter } from "huddle01-client";
 
-import HuddleClient, { emitter } from "./lib/huddle-client";
+// import HuddleClient, { emitter } from "./lib/huddle-client";
 
 import { useEffect, useState, useRef } from "react";
 import { PeerVideo, PeerAudio } from "./components/PeerViewport";
@@ -34,73 +34,57 @@ function App() {
   }, []);
 
   const setupEventListeners = async () => {
-    emitter.on("roomState", (state) => {
-      if (state === "connected") {
-        emitter.on("addPeer", (peer) => {
-          console.log("new peer =>", peer);
-          setPeers((_peers) => [..._peers, peer]);
-          console.log("setPeers", peers);
-        });
+    emitter.on("addPeer", (peer) => {
+      console.log("new peer =>", peer);
+      setPeers((_peers) => [..._peers, peer]);
+      console.log("setPeers", peers);
+    });
 
-        emitter.on("addProducer", (producer) => {
-          console.log(producer);
-          if (producer.track.kind === "video") {
-            const videoStream = producer.track;
-            if (typeof videoStream == "object") {
-              try {
-                meVideoElem.current.srcObject = getTrack(videoStream);
-              } catch (error) {
-                console.error(error);
-              }
-            }
-          } else if (producer.kind === "audio") {
-            //TODO: handle my audio producer
+    emitter.on("addProducer", (producer) => {
+      console.log(producer);
+      if (producer.track.kind === "video") {
+        const videoStream = producer.track;
+        if (typeof videoStream == "object") {
+          try {
+            meVideoElem.current.srcObject = getTrack(videoStream);
+          } catch (error) {
+            console.error(error);
           }
-        });
+        }
+      } else if (producer.kind === "audio") {
+        //TODO: handle my audio producer
+      }
+    });
 
-        emitter.on("addConsumer", (consumer) => {
-          console.log("new consumer =>", consumer);
-          if (consumer.track.kind === "video") {
-            //TODO: handle consumer screenshares
-            const videoStream = consumer.track;
+    emitter.on("addConsumer", (consumer) => {
+      console.log("new consumer =>", consumer);
+      if (consumer.track.kind === "video") {
+        //TODO: handle consumer screenshares
+        const videoStream = consumer.track;
 
-            let _peers = peers;
-            console.log({ _peers });
-            for (const _peer of _peers) {
-              if (_peer.id === consumer.peerId) {
-                console.log("yezzir");
-                _peer.consumers.push(videoStream);
-              }
-            }
-            console.log({ _peers });
-            setPeers(_peers);
-
-            console.log(peers);
-
-            setConsumerStreams((prevState) => ({
-              ...prevState,
-              video: [...prevState.video, videoStream],
-            }));
-          } else if (consumer.track.kind === "audio") {
-            const audioStream = consumer.track;
-            setConsumerStreams((prevState) => ({
-              ...prevState,
-              audio: [...prevState.audio, audioStream],
-            }));
+        let _peers = peers;
+        console.log({ _peers });
+        for (const _peer of _peers) {
+          if (_peer.id === consumer.peerId) {
+            console.log("yezzir");
+            _peer.consumers.push(videoStream);
           }
-        });
+        }
+        console.log({ _peers });
+        setPeers(_peers);
 
-        emitter.on("removeConsumer", ({ consumerId, peerId }) => {
-          // setConsumerStreams((prevState) =>
-          //   prevState.filter((consumer) => consumer.id !== consumerId)
-          // );
-        });
+        console.log(peers);
 
-        emitter.on("removePeer", (peerId) => {
-          setPeers((prevState) =>
-            prevState.filter((peer) => peer.id !== peerId)
-          );
-        });
+        setConsumerStreams((prevState) => ({
+          ...prevState,
+          video: [...prevState.video, videoStream],
+        }));
+      } else if (consumer.track.kind === "audio") {
+        const audioStream = consumer.track;
+        setConsumerStreams((prevState) => ({
+          ...prevState,
+          audio: [...prevState.audio, audioStream],
+        }));
       }
     });
   };
